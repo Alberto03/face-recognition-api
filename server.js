1cd9514b073124608f2d7515b742943ce96217b1
alberto03 <alberto.abis1@gmail.com>
2021-12-27 15:56:11 +0100

    add port

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)});
 app.put('/image', (req, res)=>{image.handleImage(req, res, db)});
 app.post('/imageUrl', (req, res) => {image.handleApiCall(req, res)});
 
-app.listen(3000, () => {
-    console.log('app running');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('app running on port ${process.env.PORT}');
 });
